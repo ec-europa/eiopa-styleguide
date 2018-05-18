695e3741e847a5fca5a05f741cdd235d17110ff2
Radu Chiriac <contact@raduchiriac.me>
2018-05-18 11:15:21 +0200

    feat(breadcrumb): add component - INNO-1065 (#9)
    * Init breadcrum component (fix)
    
    * Remove mentions of generic
    
    * Fix imports in presets
    
    * Fix component-link imports

---
+++ src/systems/eiopa/eiopa-component/eiopa-component-breadcrumb/data/demo.js
@@ -0,0 +1,16 @@
+module.exports = {
+  segments: [
+    {
+      href: '../../example.html#1',
+      label: 'European Commission',
+    },
+    {
+      href: '../../example.html#2',
+      label: 'Priorities',
+    },
+    {
+      href: '../../example.html#3',
+      label: 'Jobs, Growth and Investment',
+    },
+  ],
+};
